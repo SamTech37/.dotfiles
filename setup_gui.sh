9b2157cb03de5dd1cd0d02d2cbd3f3e762c66f6b
samtech37 <samkondori@gmail.com>
2025-12-06 19:38:08 +0800

    add GNOME settings backup and load functionality in setup script

--- setup_gui.sh
+++ setup_gui.sh
@@ -17,12 +17,20 @@ APT_PACKAGES=(
 echo "Installing packages..."
 sudo apt install -y "${APT_PACKAGES[@]}"
 
-# Read dconf settings for GNOME
-if [ -f desktop/gnome/dconf-settings.ini ]; then
-    echo "Loading GNOME settings..."
-    dconf load /org/gnome/shell/ < desktop/gnome/dconf-settings.ini
+# Read dconf settings for GNOME Shell
+if [ -f desktop/gnome/dconf-shell.ini ]; then
+    echo "Loading GNOME Shell settings..."
+    dconf load /org/gnome/shell/ < desktop/gnome/dconf-shell.ini
 else
-    echo "[WARNING] GNOME settings file 'desktop/gnome/dconf-settings.ini' not found. Skipping GNOME settings import."
+    echo "[WARNING] 'desktop/gnome/dconf-shell.ini' not found. Skipping Shell settings."
 fi 
+
+# Read dconf settings for GNOME Desktop
+if [ -f desktop/gnome/dconf-desktop.ini ]; then
+    echo "Loading GNOME Desktop settings..."
+    dconf load /org/gnome/desktop/ < desktop/gnome/dconf-desktop.ini
+else
+    echo "[WARNING] 'desktop/gnome/dconf-desktop.ini' not found. Skipping Desktop settings."
+fi
 
 echo "Setup GUI complete!"
